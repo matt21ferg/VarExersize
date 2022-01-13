b03271c3f1000173815f276228fb233a2bfa8655
matt21ferg <matt21ferguson@gmail.com>
2022-01-13 16:34:31 -0600

    1st commit

---
+++ VarExersize/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace VarExersize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "matt";
+            int age = 20;
+            char intial = 't';
+            bool married = false;
+            if (married == true)
+            {
+                name = "MR.matt";
+            }
+            double whieght = 200.6;
+            decimal hight = 129.52m;
+            Console.WriteLine($"Hi my name is {name} I'm { age} years old and my midlle intial is {intial}!"!);
+
+            Console.WriteLine($"I am {whieght} pounds and{hight}inches tall! ");
+
+
+        }
+    }
+}
